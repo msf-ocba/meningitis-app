9fe4d4b5e1ef2e138ebe4dd41dbb2629299c94f8
velasvalen17 <velasvalen17@gmail.com>
2020-02-24 11:21:16 +0100

    Buttons and forms added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,39 @@ import i18n from '@dhis2/d2-i18n'
 import { useConfig } from '@dhis2/app-runtime'
 import { useDataQuery } from '@dhis2/app-runtime'
 import { Provider } from '@dhis2/app-runtime'
+import { useDataMutation } from '@dhis2/app-runtime'
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An Org Unit was submitted ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Org Unit ID:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 const appConfig = {
     baseUrl: 'https://dev.hmisocba.msf.es',
@@ -14,16 +47,168 @@ const appConfig = {
 const query = {
     events: {
         resource: 'events.json',
+        filter: {
+            startDate: '2020-01-01'
+        },
         params: {
             orgUnit: 'wg60MeX0Txd',
             ouMode: 'DESCENDANTS',
             pageSize: 10,
+            order: 'eventDate',
         },
     },
 }
 
+const query2 = {
 
-const MyApp = () => 
+}
+
+const mutation = {
+    resource: 'indicators',
+    type: 'create',
+    data: ({ name }) => ({
+        name,
+        shortName: name,
+        indicatorType: {
+            id: 'bWuNrMHEoZ0',
+        },
+        numerator: '#{fbfJHSPpUQD}',
+        denominator: '#{h0xKKjijTdI}',
+    }),
+}
+
+const AddButton = ({ onCreate }) => {
+    const [mutate] = useDataMutation(mutation, {
+        onComplete: onCreate,
+        variables: {
+            orgUnit: 'Pyt3nwQOO6a',
+        },
+    })
+
+    return (
+        <button
+            onClick={() => {
+                mutate()
+            }}
+            style={{ margin: 10 }}
+        >
+            Add Origin Events
+        </button>
+    )
+}
+
+const AddButton2 = ({ onCreate }) => {
+    const [mutate] = useDataMutation(mutation, {
+        onComplete: onCreate,
+        variables: {
+            orgUnit: 'Pyt3nwQOO6a',
+        },
+    })
+
+    return (
+        <button
+            onClick={() => {
+                mutate()
+            }}
+            style={{ margin: 10 }}
+        >
+            Delete Origin Events
+        </button>
+    )
+}
+
+const AddButton3 = ({ onCreate }) => {
+    const [mutate] = useDataMutation(mutation, {
+        onComplete: onCreate,
+        variables: {
+            orgUnit: 'Pyt3nwQOO6a',
+        },
+    })
+
+    return (
+        <button
+            onClick={() => {
+                alert('New origin events have been created!')
+                event.preventDefault();
+            }}
+            style={{ margin: 10 }}
+        >
+            Add Origin Events
+        </button>
+    )
+}
+
+const AddButton4 = ({ onCreate }) => {
+    const [mutate] = useDataMutation(mutation, {
+        onComplete: onCreate,
+        variables: {
+            orgUnit: 'Pyt3nwQOO6a',
+        },
+    })
+
+    return (
+        <button
+            onClick={() => {
+                alert('Corrections have been made and origin events have been deleted!')
+                event.preventDefault();
+            }}
+            style={{ margin: 10 }}
+        >
+            Delete Origin Events
+        </button>
+    )
+}
+
+
+const MeningitisApp = () => {
+
+}
+
+const AskEvents = () => {
+    const { loading, error, data } = useDataQuery(query)
+    return (
+            <>
+                {/*
+                <h3>Events (first 10)</h3>
+                {loading && <span>...</span>}
+                {error && <span>{`ERROR: ${error.message}`}</span>}
+                {data && (
+                    <pre>
+                        {data.events.events
+                            .map(ev => ev.eventDate)
+                            .join('\n')}
+                    </pre>
+
+                )}
+                */}
+                <br></br>
+                <div><NameForm/></div>
+                <AddButton3/>
+                <AddButton4/>
+            </>
+
+        )
+    
+}
+
+export default AskEvents
+
+{/*
+const CheckEvent = props => {
+}
+
+const CreateEvent = props => {
+}
+
+const DeleteEvent = props => {
+}
+
+const EventController = props => {
+}
+
+
+
+{/*const MyApp = () => 
 
 
 {
@@ -48,7 +233,7 @@ const MyApp = () =>
             {/*
             <h2>Base URL : {baseUrl}</h2>
             <h2>API Version : {apiVersion}</h2>
-            */}
+            
             <h1>Events (first 10)</h1>
             {loading && <span>...</span>}
             {error && <span>{`ERROR: ${error.message}`}</span>}
